be76bd2d998da367bb9b0e27b6b06d4ebcf08d7f
Mr.Awwad <omarawwad010@gmail.com>
2018-08-10 14:21:26 -0400

    10-8

---
+++ react_cc_traversy/src/components/Projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className="Projects">
+        my Project
+      </div>
+    );
+  }
+}
+
+export default Projects;
--- weather/src/reducers/reducer_weather.js
+++ weather/src/reducers/reducer_weather.js
@@ -10,7 +10,7 @@ export default function (state = [], action) { // here we actually should consum
       // return state.push(action.payload.data) dont ever modify (change,manipulate) the state manually always return a new one 
       // return state.concat([action.payload.data]); // this is okay because it returns an new array (state)
       return [action.payload.data, ...state];
-    // using rest parameters, it gives u back an array which
+    // using the spread operator, it gives u back an array which
     // starts with action.payload.data
   }
   return state;
